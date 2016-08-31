6409becd15ae32b33a4e996663c45288efff4026
Caroline <cstewart@mercury.io>
2016-08-31 17:31:57 -0500

    initial commit

---
+++ sunshinewatch/src/main/java/com/example/android/sunshinewatch/DigitalWatchActivity.java
@@ -0,0 +1,23 @@
+//package com.example.android.sunshinewatch;
+//
+//import android.app.Activity;
+//import android.os.Bundle;
+//import android.widget.TextView;
+//
+//public class DigitalWatchActivity extends Activity {
+//
+//    private TextView mTextView;
+//
+//    @Override
+//    protected void onCreate(Bundle savedInstanceState) {
+//        super.onCreate(savedInstanceState);
+//        setContentView(R.layout.activity_main);
+////        final WatchViewStub stub = (WatchViewStub) findViewById(R.id.square);
+////        stub.setOnLayoutInflatedListener(new WatchViewStub.OnLayoutInflatedListener() {
+////            @Override
+////            public void onLayoutInflated(WatchViewStub stub) {
+////                mTextView = (TextView) stub.findViewById(R.id.text);
+////            }
+////        });
+//    }
+//}
